7c4711eca053f17e11e14d0f3b2a861353f05544
Fumihiro Itoh <fmhrit@gmail.com>
2013-10-20 03:42:41 +0900

    update dev-app cookbook
    update supervisord recipe and template

--- chef/site-cookbooks/dev-app/recipes/supervisord.rb
+++ chef/site-cookbooks/dev-app/recipes/supervisord.rb
@@ -1,10 +1,13 @@
+execute 'supervisord_update' do
+  command 'supervisorctl update'
+  user 'root'
+  action :nothing
+end
+
 template "/etc/supervisord.d/dev-app.ini" do
   source "supervisord/dev-app.conf.erb"
   mode 0644
   owner "root"
   group "root"
-end
-
-execute 'supervisorctl update' do
-  user 'root'
+  notifies :run, 'execute[supervisord_update]', :delayed
 end
