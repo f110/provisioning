6760b50bfa3980ef25a7690f3ea663e117b6e7f6
Fumihiro Itoh <fmhrit@gmail.com>
2013-10-20 03:41:40 +0900

    install ruby using rbenv

--- chef/site-cookbooks/ruby/recipes/default.rb
+++ chef/site-cookbooks/ruby/recipes/default.rb
@@ -7,6 +7,16 @@
 # All rights reserved - Do Not Redistribute
 #
 
-package 'ruby' do
+include_recipe 'rbenv::default'
+include_recipe 'rbenv::ruby_build'
+
+# install depends on package
+package "openssl-devel" do
   action :install
+end
+
+rbenv_ruby node['ruby']['version']
+
+rbenv_gem "bundler" do
+  ruby_version node['ruby']['version']
 end
